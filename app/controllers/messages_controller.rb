7a0e642f8dc549e635822a2fd5b8a2bc1dcef7b1
Dmitriy <bj911@ukr.net>
2016-04-19 17:39:08 +0300

    test webhook8

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!, except: :payload
   before_action :get_chat, except: [:search, :payload]
   require 'json'
 
