5c48e38339ea2ed06e94a294c440e756715afc13
Dmitriy <bj911@ukr.net>
2016-04-19 16:27:13 +0300

    test webhook

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get '/messages/autocomplete' => 'messages#autocomplete'
   get '/search' => 'messages#search', as: 'search'
 
+  post '/payload' => 'messages#autocomplete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
