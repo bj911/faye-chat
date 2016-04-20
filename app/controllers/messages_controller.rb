40372d623ea7f2c2d1acd8b5737f3a7e6beb63d4
Dmitriy <bj911@ukr.net>
2016-04-20 09:37:43 +0300

    fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -34,7 +34,7 @@ class MessagesController < ApplicationController
     puts '+++++++++++++++++++ 777 +++++++++++++++++++++++++++'
     push = JSON.parse(request.body.read)
     puts "I got some JSON: #{push.inspect}"
-    redirect_to '/'
+    render status: :ok
   end
 
   private
